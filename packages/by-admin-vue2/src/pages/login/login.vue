755f8be9ad92d7810dd59ccecb6f047ddd7d4634
王琦(wangqi2) <wangqi2@mochasoft.com.cn>
2024-08-06 14:44:34 +0800

    feat(vue2): 更新登录页面

---
+++ packages/by-admin-vue2/src/pages/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {},
+
+  mounted() {},
+
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
+
