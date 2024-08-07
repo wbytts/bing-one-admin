5c34d445bedeca7b70fad2e060f93a7a398e84a7
王琦(wangqi2) <wangqi2@mochasoft.com.cn>
2024-08-07 10:52:55 +0800

    feature(wq): vue2-admin-template项目vuex状态管理注释

--- some-projects/vue-admin-template/src/store/modules/app.js
+++ some-projects/vue-admin-template/src/store/modules/app.js
@@ -1,5 +1,15 @@
 import Cookies from 'js-cookie'
 
+// store 仓库
+// state 存储状态
+// mutation 改变状态（同步）
+// action 改变状态（异步）
+
+// 调用 Mutation -> store.commit(mutation名称, mutation负载)
+// 调用 Action -> store.dispatch(action名称, action负载)
+
+// 视图 -> 事件 -> 调用Action -> 调用Mutation -> 修改State -> 响应式数据改变重新渲染视图
+
 const state = {
   sidebar: {
     opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
@@ -29,8 +39,8 @@ const mutations = {
 }
 
 const actions = {
-  toggleSideBar({ commit }) {
-    commit('TOGGLE_SIDEBAR')
+  toggleSideBar(context) {
+    context.commit('TOGGLE_SIDEBAR')
   },
   closeSideBar({ commit }, { withoutAnimation }) {
     commit('CLOSE_SIDEBAR', withoutAnimation)
